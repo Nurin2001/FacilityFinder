67a8011614da384c570186e2dd0032f5f763abf3
Nurin2001 <a182071@siswa.ukm.edu.my>
2023-11-04 10:32:33 +0800

    edit profile

--- app/src/main/java/com/assignment/facilityfinder/EditProfileActivity.java
+++ app/src/main/java/com/assignment/facilityfinder/EditProfileActivity.java
@@ -3,12 +3,16 @@ package com.assignment.facilityfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    private EditText nameEt, emailEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
     }
 }
